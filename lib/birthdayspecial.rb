a02135b4f91fdb62363d25f1877daf7ef12fee82
Pushkar Singh <psrathore820909@gmail.com>
2023-07-06 14:19:52 +0530

    initial commmit

---
+++ lib/birthdayspecial.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "birthdayspecial/version"
+
+module Birthdayspecial
+  class Error < StandardError; end
+  def wishing 
+        puts "Happy birthday !! "
+  end 
+end
---
+++ lib/birthdayspecial/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Birthdayspecial
+  VERSION = "0.1.0"
+end
